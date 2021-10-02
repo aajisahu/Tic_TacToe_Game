046e7de344264962cc5eae804ed904b2b1b856ee
Ashish Sahu <aajisahu@gmail.com>
2021-10-02 15:10:38 +0530

    adding: allow player to choose

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,21 +1,51 @@
 package com.tiktactoe;
-
+import java.util.*;
 public class TicTacToeGame {
-	 static  char[] board = new char[10];
 
-	    public static char[]  creatingBoard() {
+    static  char[] board = new char[10];
+    private static char cross ='X';
+    private static char zero ='O';
+    private static char player;
+    private static char computer;
+    public static char[]  creatingBoard() {
 
 
-	        for (int i = 1; i < board.length; i++) {
-	            board[i] = ' ';
-	        }
-	        return board;
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+        return board;
+    }
+    private static void allowPlayerToChoose()
+    {
+        System.out.println("Enter X or O you want to choose");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        player = input.charAt(0);
 
-	    }
+        if(player == cross)
+        {
+            player = cross;
+            computer = zero;
+            System.out.println("player symbol : " + player);
 
-	    public static void main(String args[]) {
+        }
+        else if(player == zero)
+        {
+            player = zero;
+            computer = cross;
+            System.out.println("player symbol : " + player);
 
-	        System.out.println("Welcome to TicTacToe Board Game");
-	        board = creatingBoard();
-	    }
+        }
+        else
+        {
+            System.out.println("Invalid Character");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println("Welcome to TicTacToe Board Game");
+        board = creatingBoard();
+        allowPlayerToChoose();
+    }
 }
